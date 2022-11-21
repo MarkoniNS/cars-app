8e4d55c9dd5ef0c319120958169e01495b813877
vivify <vivify@vivifys-MacBook-Pro-4.local>
2022-11-21 19:15:59 +0100

    Commit 3

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Model\Posts;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+ 
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/posts', function () {
+    $posts = Post::where('published', 1)->get();
+
+    return view('posts', compact('posts'));
+});
+
+Route::get('/posts/{id}', function ($id) {
+    $post = Post::find($id);
+
+    return view('post', compast('post'));
+})->name('single-post');
